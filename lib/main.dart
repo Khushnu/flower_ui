f62634d1390caf8d36e8dbccaae0856b8bb035f0
Your Name <you@example.com>
2022-12-20 15:52:37 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(LoginUi());
+}
+
+class LoginUi extends StatefulWidget {
+  const LoginUi({super.key});
+
+  @override
+  State<LoginUi> createState() => _LoginUiState();
+}
+
+class _LoginUiState extends State<LoginUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
